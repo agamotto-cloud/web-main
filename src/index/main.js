115269fa7b127cacc5ab138d7af004a7cb97c40a
董文强 <826112946@qq.com>
2023-03-02 23:12:20 +0800

    页面基本框架

--- src/index/main.js
+++ src/index/main.js
@@ -3,18 +3,49 @@ import './index.css'
 
 
 
+const sidebar = document.querySelector(".sidebar");
 
-const sidebar = document.getElementById("main-menu");
+const menu = document.getElementById("main-menu");
 const toggleBtn = document.querySelector(".toggle-sidebar");
 const mainBodyAll = document.querySelectorAll(".main-right-body");
 
 
 console.log(toggleBtn)
-toggleBtn.addEventListener("click", function() {
-  mainBodyAll.forEach(e=>e.classList.toggle("collapsed"))
+toggleBtn.addEventListener("click", function () {
+  mainBodyAll.forEach(e => e.classList.toggle("collapsed"))
   sidebar.classList.toggle("collapsed");
   console.log(toggleBtn.children[0])
   toggleBtn.children[0].classList.toggle('rotate-text');
 });
 
 // 
+
+
+// 获取video元素
+const video = document.querySelector('video');
+
+// 请求用户授权访问摄像头
+navigator.mediaDevices.getUserMedia({ video: true })
+  .then(stream => {
+    // 将视频流绑定到video元素
+    video.srcObject = stream;
+    video.play();
+  })
+  .catch(error => {
+    console.log('获取摄像头画面失败:', error);
+  });
+
+
+
+
+
+for (let i = 0; i < 30; i++) {
+ 
+var newDiv = document.createElement("div");
+var a = document.createElement('a');
+a.href = "#home"+i
+a.innerHTML = "home"+i;
+newDiv.appendChild(a)
+menu.appendChild(newDiv);
+  
+}
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
+/*
+ * @Author: 董文强 826112946@qq.com
+ * @Date: 2023-02-20 09:07:27
+ * @LastEditors: 董文强 826112946@qq.com
+ * @LastEditTime: 2023-02-20 09:08:56
+ * @FilePath: \web-main\vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
+  server: {
+    host: '0.0.0.0'
+  },
   build: {
     rollupOptions: {
       input: {
