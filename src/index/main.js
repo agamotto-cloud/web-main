dc5ac9243815d8e4a40ed2291dc5d849a2f0e7f4
董文强 <826112946@qq.com>
2023-03-08 16:27:36 +0800

    fix: 添加点击后菜单样式变化

--- src/index/main.js
+++ src/index/main.js
@@ -1,11 +1,3 @@
-/*
- * @Author: 董文强 826112946@qq.com
- * @Date: 2023-02-20 09:07:27
- * @LastEditors: 董文强 826112946@qq.com
- * @LastEditTime: 2023-03-06 23:38:11
- * @FilePath: \web-main\src\index\main.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 
 import './index.css'
 
@@ -47,12 +39,19 @@ navigator.mediaDevices.getUserMedia({ video: true })
 
 const menu = document.getElementById("main-menu");
 
-
+var menuLi = [];
 function addMenu(menuUl,item){
   var newLi = document.createElement("li");
+  menuLi.push(newLi)
   var a = document.createElement('a');
   a.href = item.path
   a.innerHTML = item.name;
+  newLi.addEventListener("click",()=>{
+    menuLi.forEach(item => {
+      item.classList.remove('active');
+    });
+    newLi.classList.add("active")
+  })
   newLi.appendChild(a)
   menuUl.appendChild(newLi);
 }
